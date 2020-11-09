4b2cdf35fbe9072bf111b4fe0aeac7b0c65b98e0
Marta Trusohamn <marta.trusohamn@gmail.com>
2020-11-09 11:31:16 +0100

    init

---
+++ react-web/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import MapLeaflet from "mapleaflet-react-web-native";
+
+function App() {
+  return (
+    <div className="App">
+      <MapLeaflet />
+    </div>
+  );
+}
+
+export default App;
