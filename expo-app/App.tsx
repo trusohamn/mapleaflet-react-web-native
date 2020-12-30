394d948e0f86c7a2a7cecb4c3740fb16c6053518
Marta Trusohamn <marta.trusohamn@gmail.com>
2020-12-30 21:40:47 +0100

    imported map

--- expo-app/App.tsx
+++ expo-app/App.tsx
@@ -1,11 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import MapLeaflet from "mapleaflet-react-web-native";
+
+const markers = [
+  {
+    id: "1",
+    position: { lat: 59.5, lng: 18.0 },
+    size: [32, 32],
+  },
+  {
+    id: "2",
+    position: { lat: 59.56, lng: 18.0 },
+    size: [32, 32],
+  },
+  {
+    id: "3",
+    position: { lat: 59.59, lng: 18.0 },
+    size: [32, 32],
+  },
+];
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <MapLeaflet markers={markers} />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +33,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
